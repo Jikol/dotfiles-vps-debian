1224d01850fa2a7271fc05b9d5e6c73e6463389f
Ondřej Javorský <gikol113@gmail.com>
2026-03-14 19:41:40 +0000

    feat: remove vim alias, add nvim symlink to sudo wrapper

--- dotconfig/fish/conf.d/helpers.fish
+++ dotconfig/fish/conf.d/helpers.fish
@@ -1,11 +1,13 @@
 ## Functions ##
 function sudo-wrapper
-	if contains -- "$argv[1]" apt
-		command sudo nala $argv[2..-1]
-	else
-		command sudo $argv
-	end
-end
+  if contains -- "$argv[1]" apt
+    command sudo nala $argv[2..-1]
+  else if contains -- "$argv[1]" nvim vim
+    command sudo HOME=$HOME "$argv[1]" $argv[2..-1]
+  else
+    command sudo $argv
+  end
+ end
 
 function apt-wrapper
 	command nala $argv
