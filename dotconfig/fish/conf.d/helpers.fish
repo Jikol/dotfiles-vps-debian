8e363c7275952b84bb3c45d798f6e5fde4d58e07
Ondřej Javorský <gikol113@gmail.com>
2026-05-09 01:37:54 +0000

    feat: switch to native linux ssh
    - Remove Windows SSH wrapper function from fish config
    - Update git sshCommand from Windows path to /usr/bin/ssh
    - Update gpg ssh-keygen program from Windows to /usr/bin/ssh-keygen

--- dotconfig/fish/conf.d/helpers.fish
+++ dotconfig/fish/conf.d/helpers.fish
@@ -13,28 +13,9 @@ function apt-wrapper
 	command nala $argv
 end
 
-function ssh-wrapper
-  if test (count $argv) -ge 1
-    if contains -- "$argv[1]" ls list
-      set configPath "/mnt/c/Users/Jikol/.ssh/config"
-      if test -f $configPath
-        grep -E '^\s*(Host|HostName)\s+' $configPath | 
-        while read -l line
-          echo $line
-        end
-      else
-        echo "No SSH config file found at $configPath"
-      end
-    else
-      command ssh.exe $argv
-    end
-  end
-end
-
 ## Aliases ##
 alias sudo="sudo-wrapper"
 alias apt="apt-wrapper"
-alias ssh="ssh-wrapper"
 
 alias batrld="batcat cache --build"
 alias tmuxrld="tmux source $HOME/.config/tmux/tmux.conf"
